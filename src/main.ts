0e1ff2877540610dcb7395cc1d4145fa9e47294f
Arjun Chib <arjunpatrickchib@gmail.com>
2023-09-18 14:57:09 -0400

    Auto-populate secrets

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { AddApplicationCommand } from "./commands/add.command";
 import { AddController } from "./controllers/add.controller";
-import { NameRouter, bootstrap } from "blurp";
+import { Router, bootstrap } from "blurp";
 
 bootstrap({
   commands: [AddApplicationCommand],
-  router: new NameRouter([AddController]),
-  applicationId: Bun.env.APPLICATION_ID!,
-  botToken: Bun.env.BOT_TOKEN!,
-  publicKey: Bun.env.PUBLIC_KEY!,
+  router: new Router([AddController]),
   guilds: [Bun.env.GUILD_ID!],
   port: 8787,
 });
